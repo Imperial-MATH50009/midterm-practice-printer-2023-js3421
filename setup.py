e43b09dd1596dac169813e676280d5addf89e919
js3421 <149166200+js3421@users.noreply.github.com>
2023-11-04 11:21:32 +0800

    commit final file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='printer',
+    version='1.0.0',
+    author='sjy',
+    author_email='js3421@ic.ac.uk',
+    description='printer',
+    packages=['printer'],
+)
